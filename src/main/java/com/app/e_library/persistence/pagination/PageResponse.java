d4dbc49785b6d3473672c7db60756c1c963705e3
VladimirMargaryan <vlad.margaryan98@gmail.com>
2022-06-04 00:43:42 +0400

    .

--- src/main/java/com/app/e_library/persistence/pagination/PageResponse.java
+++ src/main/java/com/app/e_library/persistence/pagination/PageResponse.java
@@ -15,7 +15,6 @@ public class PageResponse<T> {
 
     private final List<T> content;
     private final Integer totalPages;
-    private final Long totalElements;
 
     @JsonIgnore
     private final HttpHeaders pageHeaders;
@@ -24,58 +23,48 @@ public class PageResponse<T> {
     public PageResponse(Page<T> page) {
         this.content = page.getContent();
         this.totalPages = page.getTotalPages();
-        this.totalElements = page.getTotalElements();
         this.pageHeaders = buildPageHeaders(page);
     }
 
-    private String constructNextPageUrl(Page<T> page){
-        return page.hasNext() ? ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .replaceQueryParam("page", page.getNumber() + 1)
-                .build().toUriString() : null;
-    }
-
-    private String constructPreviousPageUrl(Page<T> page){
-        return page.hasPrevious() ? ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .replaceQueryParam("page", page.getNumber() - 1)
-                .build().toUriString() : null;
-    }
-
-    private String constructFirstPageUrl(Page<T> page){
-        return !page.isFirst() && page.hasPrevious() ? ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .replaceQueryParam("page", 0)
-                .build().toUriString() : null;
-    }
-
-    private String constructLastPageUrl(Page<T> page){
-        return !page.isLast() && page.hasNext() ? ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .replaceQueryParam("page", page.getTotalPages() - 1)
-                .build().toUriString() : null;
-    }
 
     private HttpHeaders buildPageHeaders(Page<T> page){
-
-        String firstPageUrl = constructFirstPageUrl(page);
-        String lastPageUrl = constructLastPageUrl(page);
-        String nextPageUrl = constructNextPageUrl(page);
-        String previousPageUrl = constructPreviousPageUrl(page);
-
         MultiValueMap<String, String> pageHeadersMap = new LinkedMultiValueMap<>();
 
-        if (firstPageUrl != null)
+        if (!page.isFirst() && page.hasPrevious()) {
+            String firstPageUrl = ServletUriComponentsBuilder
+                    .fromCurrentRequest()
+                    .replaceQueryParam("page", 0)
+                    .build().toUriString();
+
             pageHeadersMap.add("first", firstPageUrl);
+        }
 
-        if (lastPageUrl != null)
+        if (!page.isLast() && page.hasNext()) {
+            String lastPageUrl = ServletUriComponentsBuilder
+                    .fromCurrentRequest()
+                    .replaceQueryParam("page", page.getTotalPages() - 1)
+                    .build().toUriString();
+
             pageHeadersMap.add("last", lastPageUrl);
+        }
 
-        if (previousPageUrl != null)
+        if (page.hasPrevious()) {
+            String previousPageUrl = ServletUriComponentsBuilder
+                    .fromCurrentRequest()
+                    .replaceQueryParam("page", page.getNumber() - 1)
+                    .build().toUriString();
+
             pageHeadersMap.add("prev", previousPageUrl);
+        }
 
-        if (nextPageUrl != null)
+        if (page.hasNext()) {
+            String nextPageUrl = ServletUriComponentsBuilder
+                    .fromCurrentRequest()
+                    .replaceQueryParam("page", page.getNumber() + 1)
+                    .build().toUriString();
+
             pageHeadersMap.add("next", nextPageUrl);
+        }
 
         return new HttpHeaders(pageHeadersMap);
     }
